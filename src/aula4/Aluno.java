ef2a839c3890c37a8b26e034374fdd4c5643923e
Fabio Rodrigo Colombini <fabiorcolombini@gmail.com>
2023-09-05 16:04:52 -0300

    Aula 4

---
+++ src/aula4/Aluno.java
@@ -0,0 +1,15 @@
+package aula4;
+
+public class Aluno {
+    private String ra;
+    private String nome;
+    private String curso;
+    
+    public String getRa() {
+        return ra;
+    }
+    
+    public void setRa(String ra) {
+        this.ra = ra;
+    }
+}
