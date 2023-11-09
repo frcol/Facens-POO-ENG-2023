56ea5bc0b8b319b13999fd2e18f5c8e99ddfeab9
Fabio Rodrigo Colombini <fabiorcolombini@gmail.com>
2023-11-09 09:52:19 -0300

    Aula 13

--- src/aula14/views/Main.java
+++ src/aula14/views/Main.java
@@ -2,14 +2,25 @@ package aula14.views;
 
 import aula14.dao.UsuarioDAO;
 import aula14.entity.Usuario;
+import java.util.ArrayList;
 
 public class Main {
     public static void main(String[] args) {
+        // CRUD (Create, Read, Updade e Delete)
+        // Create
         Usuario usu = new Usuario(0, "fabio", "123");
         
         UsuarioDAO usuDao = new UsuarioDAO();
         usuDao.inserir(usu);
         
+        // Editar (Edit)
+        Usuario usu1 = new Usuario(2, "fabio", "123");
+        usuDao.editar(usu1);
         
+        // EXcluir (Delete)
+        usuDao.excluir(2);
+        
+        // Mostrar todos (Read)
+        ArrayList<Usuario> lstusuarios = usuDao.getAllUsuarios();
     }
 }
